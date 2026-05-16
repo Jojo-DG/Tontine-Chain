15ed59bf8386fca306794f5c2e25ecdcb63815ae
octavebahoun <octavebahoun@gmail.com>
2026-05-16 11:23:15 +0100

    Ajout de l'url de fetch

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_BASE_URL: "http://localhost:3000/api/v1",
+  API_BASE_URL: "https://miabehackaton-production.up.railway.app/api/v1",
   POLYGON_RPC: "https://polygon-amoy.g.alchemy.com/v2/zddKKpAtQOf3ebP4GdpiH",
   POLYGON_CHAIN_ID: 80002,
   POLYGONSCAN_URL: "https://amoy.polygonscan.com",
