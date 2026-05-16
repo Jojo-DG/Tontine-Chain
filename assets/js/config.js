0b35b9584cd8c4df43f66f3ed43b7568ef304ef3
octavebahoun <octavebahoun@gmail.com>
2026-05-16 02:55:04 +0100

    Plus ultra

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   API_BASE_URL: "https://miabehackaton-production.up.railway.app/api/v1",
-  POLYGON_RPC: "https://rpc-mumbai.maticvigil.com",
+  POLYGON_RPC: "https://polygon-amoy.g.alchemy.com/v2/zddKKpAtQOf3ebP4GdpiH",
   POLYGON_CHAIN_ID: 80001,
-  POLYGONSCAN_URL: "https://mumbai.polygonscan.com",
+  POLYGONSCAN_URL: "https://amoy.polygonscan.com",
 };
